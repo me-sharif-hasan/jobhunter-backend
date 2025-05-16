e977e2f554a8d323742e166bd86a03c8d7553052
Sharif Hasan <me.sharif.hasan@gmail.com>
2025-05-16 10:36:06 +0600

    fix: Added job normalization issues

--- src/main/java/com/iishanto/jobhunterbackend/infrastructure/repository/JobsRepository.java
+++ src/main/java/com/iishanto/jobhunterbackend/infrastructure/repository/JobsRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JobsRepository extends JpaRepository<Jobs, String> {
     @Query(value = "SELECT * FROM jobs,site WHERE site.is_duplicate = false AND (site.id = site_id AND site_id IN :siteIds AND (title LIKE %:keyword% OR job_description LIKE %:keyword% OR site.name LIKE %:keyword%)) ORDER BY job_parsed_at DESC", nativeQuery = true)
@@ -22,4 +23,6 @@ public interface JobsRepository extends JpaRepository<Jobs, String> {
     List<Jobs> findAllByJobIdIn(List<String> jobIds);
 
     List<Jobs> findAllByJobDescriptionContainingOrTitleContainingOrLocationContainingOrderByJobParsedAtDesc(String description, String title, String location, Pageable pageable);
+
+    Optional<Jobs> findByJobUrl(String jobUrl);
 }
