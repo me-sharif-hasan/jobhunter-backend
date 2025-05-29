5358ea02d57d901c56bb55210a6377a8646e67b8
Sharif Hasan <me.sharif.hasan@gmail.com>
2025-05-29 07:17:22 +0600

    bugy

--- lib/features/profile/controller/facebook_controller.dart
+++ lib/features/profile/controller/facebook_controller.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_login_facebook/flutter_login_facebook.dart';
+// import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 import 'package:get_it/get_it.dart';
 import 'package:personalized_job_hunter/features/profile/datasource/profile_datasource.dart';
 import 'package:personalized_job_hunter/util/http/client.dart';
@@ -15,22 +15,22 @@ class FacebookController extends ChangeNotifier {
   FacebookController(){
     _profileDatasource = locator<ProfileDatasource>();
   }
-  final fb = FacebookLogin();
+  // final fb = FacebookLogin();
   Future connectWithFacebook() async{
-    final res = await fb.logIn(permissions: [
-      FacebookPermission.publicProfile,
-      FacebookPermission.email,
-      FacebookPermission.pagesReadEngagement,
-      FacebookPermission.userManagedGroups
-    ]);
-    if(res.status==FacebookLoginStatus.success){
-      String? accessToken = res.accessToken?.token;
-      if(accessToken!=null){
-        log("Access Token: "+accessToken);
-        await _profileDatasource.connectFacebook(accessToken);
-      }
-    }else{
-      throw Exception("Facebook login failed");
-    }
+    // final res = await fb.logIn(permissions: [
+      // FacebookPermission.publicProfile,
+      // FacebookPermission.email,
+      // FacebookPermission.pagesReadEngagement,
+      // FacebookPermission.userManagedGroups
+    // ]);
+    // if(res.status==FacebookLoginStatus.success){
+    //   String? accessToken = res.accessToken?.token;
+    //   if(accessToken!=null){
+    //     log("Access Token: "+accessToken);
+    //     await _profileDatasource.connectFacebook(accessToken);
+    //   }
+    // }else{
+    //   throw Exception("Facebook login failed");
+    // }
   }
 }
