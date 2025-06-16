03ae3def62050be88f8aaf557c0c86b2a5dfdef4
Sharif Hasan <me.sharif.hasan@gmail.com>
2025-06-16 08:31:17 +0600

    fix: Fixed admin add site feature - no args constructor issue

--- src/main/java/com/iishanto/jobhunterbackend/web/dto/request/JobUpdateDto.java
+++ src/main/java/com/iishanto/jobhunterbackend/web/dto/request/JobUpdateDto.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class JobUpdateDto {
