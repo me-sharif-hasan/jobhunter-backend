87d4af9d1e2fa406cdf96971a927c391bea5565d
Sharif Hasan <me.sharif.hasan@gmail.com>
2025-06-18 16:17:10 +0600

    ci: Enabled test run during docker image build to have integrity always

--- src/test/java/com/iishanto/jobhunterbackend/JobAggragatorApplicationTests.java
+++ src/test/java/com/iishanto/jobhunterbackend/JobAggragatorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JobAggragatorApplicationTests {
+class JobHunterBackendTests {
 
     @Test
     void contextLoads() {
